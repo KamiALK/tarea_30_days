9d77305ee5c5f7325ad7a3f14cef9f32b2a16bab
KamiALK <69237651+kmialk@users.noreply.github.comm>
2023-12-07 21:39:13 +0100

    punto dos

--- python/dia_cuatro.py
+++ python/dia_cuatro.py
@@ -2,3 +2,7 @@ import numpy as np
  
 
 # Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'
+concatenacion = 'Thirty'+'Days'+ 'Of'+'Python'
+
+# Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+Coding_For_All = 'Coding' + 'For' + 'All'
