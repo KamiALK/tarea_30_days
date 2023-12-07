fbf951e31738324e640b43e2d1c452c409612d58
KamiALK <69237651+kmialk@users.noreply.github.comm>
2023-12-07 21:43:07 +0100

    punto tres

--- python/dia_cuatro.py
+++ python/dia_cuatro.py
@@ -6,3 +6,6 @@ concatenacion = 'Thirty'+'Days'+ 'Of'+'Python'
 
 # Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 Coding_For_All = 'Coding' + 'For' + 'All'
+
+# Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All".
