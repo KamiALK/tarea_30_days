b4deecdd8a15d0658151f832571387e7ab5e1126
KamiALK <69237651+kmialk@users.noreply.github.comm>
2023-12-13 17:23:25 -0500

    dia cuatro

--- python/dia_cuatro.py
+++ python/dia_cuatro.py
@@ -41,6 +41,40 @@ frase = 'Coding For All'
 
     
     
-parte_cambiada = input('vamos a reemplazar la frase: Coding For All /n escribe la palabra que deseas remplazar')
-parte_nueva = input('dime la parte nueva')
-print(frase.replace(parte_cambiada,parte_nueva))
+# parte_cambiada = input('vamos a reemplazar la frase: Coding For All /n escribe la palabra que deseas remplazar')
+# parte_nueva = input('dime la parte nueva')
+# print(frase.replace(parte_cambiada,parte_nueva))
+# Change Python for Everyone to Python for All using the replace method or other methods
+base = 'Python for Everyone'
+cambio = 'Python for All'
+print ('frase original',base)
+print(base.replace('Python for Everyone','Python for All'))
+
+# Split the string 'Coding For All' using space as the separator (split())
+y = 'Coding For All'
+print(y.split(' '))
+# "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+var = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(var.split(','))
+# What is the character at index 0 in the string Coding For All
+var ='Coding For All'
+indexvar = var[0]
+print(indexvar)
+
+# What is the last index of the string Coding For All.
+var ='Coding For AlL'
+ultimoindex = var[-1]
+print(ultimoindex)
+
+# What character is at index 10 in "Coding For All" string
+var ='Coding For AlL'
+index10 = var[10]
+print(index10)
+if index10 == ' ':
+    print('tomaste in espacio')
+    
+# Create an acronym or an abbreviation for the name 'Python For Everyone'
+frase = 'Python For Everyone'
+acronimo = frase[0:17:-7]
+print(acronimo)
+
